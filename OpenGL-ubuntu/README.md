9d802170687e0cb24f8f2715d724e9545b7ac25c
shevalda <13516134@std.stei.itb.ac.id>
2019-04-30 19:58:46 +0700

    fix ubuntu

---
+++ OpenGL-ubuntu/README.md
@@ -0,0 +1,35 @@
+# OpenGL for Ubuntu
+
+Tested in Ubuntu 17.04
+
+## Install Dependencies (GLFW)
+
+1. Download GLFW **source package** from [here](https://www.glfw.org/download.html)
+
+2. Go to **glfw-3.x.x** directory
+
+3. `cmake -G "Unix Makefiles"`
+
+4. Try the one of the following. If one has worked, proceed to step 5
+
+`sudo apt-get build-dep glfw`
+
+or 
+
+`sudo apt-get build-dep glfw3`
+
+or
+
+`sudo apt-get install cmake xorg-dev libglu1-mesa-dev`
+
+5. `make` (may require `sudo`)
+
+6. `make install` (may require `sudo`)
+
+## How to run
+
+1. Go to the directory of where `main.cpp` is.
+
+2. `make`
+
+3. A window should be opening
---
+++ OpenGL-ubuntu/main.cpp
@@ -0,0 +1,169 @@
+#include "glad.h"
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+#include "Shader.h"
+#include "unicorn.h"
+
+void setupAndConfigureVertex();
+void renderLoop(Shader shader);
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+// settings
+const unsigned int SCR_WIDTH = 1500;
+const unsigned int SCR_HEIGHT = 950;
+
+// global variables
+GLFWwindow* window;
+unsigned int VBO, VAO;
+//float vertices[] = {
+//	// first triangle
+//	-0.9f, -0.5f, 0.0f,		1.0f, 0.0f, 0.0f, // left 
+//	-0.0f, -0.5f, 0.0f,		0.0f, 1.0f, 0.0f, // right
+//	-0.45f, 0.5f, 0.0f,		0.0f, 0.0f, 1.0f,  // top 
+//
+//	// second triangle
+//	 0.0f, -0.5f, 0.0f,		0.0f, 1.0f, 1.0f, // left
+//	 0.9f, -0.5f, 0.0f,		1.0f, 1.0f, 0.0f, // right
+//	 0.45f, 0.5f, 0.0f,		1.0f, 0.0f, 1.0f  // top 
+//};
+int vertexCount = sizeof(vertices) / (3 * sizeof(float));
+
+
+int main()
+{
+	// glfw: initialize and configure
+	// ------------------------------
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// glfw window creation
+	// --------------------
+	window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// glad: load all OpenGL function pointers
+	// ---------------------------------------
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	// build and compile our shader program
+	// ------------------------------------
+	Shader shader("shader.vs", "shader.fs"); // you can name your shader files however you like
+	// buildCompileShaderProgram();
+
+	//float vertices[] = {
+	//	-0.5f, -0.5f, 0.0f, // left  
+	//	 0.5f, -0.5f, 0.0f, // right 
+	//	 0.0f,  0.5f, 0.0f  // top   
+	//};
+
+	setupAndConfigureVertex();
+
+	// uncomment this call to draw in wireframe polygons.
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	renderLoop(shader);
+
+	// optional: de-allocate all resources once they've outlived their purpose:
+	// ------------------------------------------------------------------------
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+
+	// glfw: terminate, clearing all previously allocated GLFW resources.
+	// ------------------------------------------------------------------
+	glfwTerminate();
+	return 0;
+}
+
+
+// set up vertex data (and buffer(s)) and configure vertex attributes
+// ------------------------------------------------------------------
+void setupAndConfigureVertex()
+{
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	// color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+	/* glBindVertexArray(0); */
+}
+
+
+// render loop
+// -----------
+void renderLoop(Shader shader)
+{
+	while (!glfwWindowShouldClose(window))
+	{
+		// input
+		// -----
+		processInput(window);
+
+		// render
+		// ------
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// draw our first triangle
+		/* glUseProgram(shaderProgram); */
+		shader.use();
+
+		glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+		glDrawArrays(GL_TRIANGLES, 0, vertexCount);
+		// glBindVertexArray(0); // no need to unbind it every time 
+
+		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+		// -------------------------------------------------------------------------------
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+}
+
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
+}
